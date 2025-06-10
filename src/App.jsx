9c80af77624c962ef7cd56e1b599f26ac7b492ba
Saidatou <babasaidatou@yahoo.fr>
2025-06-10 19:21:18 +0200

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import ImageGenerator from './Components/ImageGenerator/ImageGenerator'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <ImageGenerator/>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
